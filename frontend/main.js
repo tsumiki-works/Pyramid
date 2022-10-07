96a69a005e9cece399358f85d7fa3c5e1bd468c6
mugicha9 <jkn.mugicha@gmail.com>
2022-10-07 23:18:31 +0900

    unify new functions

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,3 @@
-//ああああああああああああああああああああ
-
 // Global constants
 const workspace = document.getElementById("workspace");
 const generator = document.getElementById("generator");
@@ -10,7 +8,10 @@ const BLOCK_W = 2;
 const BLOCK_H = 3;
 const BLOCK_PARENT = 4;
 const BLOCK_CHILDREN = 5;
-const BLOCK_ELEM = 6;
+const BLOCK_ID = 6;
+const BLOCK_TYPE = 7;
+const BLOCK_CHILDREN_NUM = 8;
+const BLOCK_ELEM = 9;
 
 // Global variants
 let blocks = [];
@@ -19,7 +20,10 @@ let y_dragstart = 0;
 let listener_move = null;
 let listener_up = null;
 
-function createBlock(x, y, w, h) {
+// Temp variants for BLOCK_ID
+let block_num = 0;
+
+function createBlock(x, y, w, h, t, n) {
     const elem = document.createElement("div");
     elem.style.left = x + "px";
     elem.style.top = y + "px";
@@ -33,18 +37,34 @@ function createBlock(x, y, w, h) {
     block.push(h);
     block.push(null);
     block.push([]);
+    block.push(block_num);
+    block.push(t);
+    block.push(n);
     block.push(elem);
     elem.onmousedown = event => funBlockOnMouseDown(block, event);
     workspace.appendChild(elem);
+
+    block_num += 1;
+
     return block;
 }
 
 function getCenterX(block) {
-    return block[BLOCK_X] - block[BLOCK_W] / 2.0;
+    return block[BLOCK_X] + block[BLOCK_W] / 2.0;
 }
 
 function getCenterY(block) {
-    return block[BLOCK_Y] - block[BLOCK_H] / 2.0;
+    return block[BLOCK_Y] + block[BLOCK_H] / 2.0;
+}
+
+//n: input split, idx: index
+function getSplitCenterX(block, idx) {
+    let ret = -1;
+    let n = block[BLOCK_CHILDREN_NUM];
+    if(n > 0 && idx < n){
+        ret = block[BLOCK_X] + (((block[BLOCK_W] / (2 * n))) * ((2 * idx) + 1));
+    }
+    return ret 
 }
 
 function updatePosition(block, x, y) {
@@ -53,6 +73,8 @@ function updatePosition(block, x, y) {
     block[BLOCK_ELEM].style.left = x + "px";
     block[BLOCK_ELEM].style.top = y + "px";
 }
+
+// Event/Block
 
 function funBlockOnMouseDown(block, event) {
     const rect = block[BLOCK_ELEM].getBoundingClientRect();
@@ -82,25 +104,72 @@ function funBlockOnMouseUp(block, event) {
     for (let i = 0; i < blocks.length; ++i) {
         if (blocks[i] === block)
             continue;
-        if ((getCenterX(block) - getCenterX(blocks[i])) ** 2 >= Math.min(block[BLOCK_W] / 2.0, blocks[i][BLOCK_W] / 2.0) ** 2)
-            continue;
         if ((getCenterY(block) - getCenterY(blocks[i])) ** 2 >= Math.min(block[BLOCK_H], blocks[i][BLOCK_H]) ** 2)
             continue;
+
+        // (parent, child) = (block[i], block)
         if (getCenterY(block) > getCenterY(blocks[i])) {
-            updatePosition(block, getCenterX(blocks[i]) + block[BLOCK_W] / 2.0, blocks[i][BLOCK_Y] + blocks[i][BLOCK_H]);
-            blocks[i][BLOCK_CHILDREN].push(block);
-            block[BLOCK_PARENT] = blocks[i];
-            alert("connected");
+            // Search nearest parent's connection
+            for(let j = 0; j < blocks[i][BLOCK_CHILDREN_NUM]; ++j){
+                if ((getCenterX(block) - getSplitCenterX(blocks[i], j)) ** 2 >= Math.min(block[BLOCK_W] / 2.0, blocks[i][BLOCK_W] / 2.0) ** 2)
+                    continue;
+                updatePosition(block, getSplitCenterX(blocks[i], j) - block[BLOCK_W] / 2.0, blocks[i][BLOCK_Y] + blocks[i][BLOCK_H]);
+                blocks[i][BLOCK_CHILDREN].push(block);
+                block[BLOCK_PARENT] = blocks[i];
+                //alert("connected");
+            }
         } else {
-            updatePosition(block, getCenterX(blocks[i]) + block[BLOCK_W] / 2.0, blocks[i][BLOCK_Y] - block[BLOCK_H]);
-            block[BLOCK_CHILDREN].push(blocks[i]);
-            blocks[i][BLOCK_PARENT] = block;
-            alert("connected");
+            if((getCenterX(block) - getCenterX(blocks[i])) ** 2 <= Math.min(block[BLOCK_W] / 2.0, blocks[i][BLOCK_W] / 2.0) ** 2){
+                updatePosition(block, getCenterX(blocks[i]) - block[BLOCK_W] / 2.0, blocks[i][BLOCK_Y] - block[BLOCK_H]);
+                block[BLOCK_CHILDREN].push(blocks[i]);
+                blocks[i][BLOCK_PARENT] = block;
+                //alert("connected");
+            }
         }
     }
     document.removeEventListener("mousemove", listener_move, false);
     document.removeEventListener("mouseup", listener_up, false);
     document.removeEventListener("mouseleave", listener_up, false);
+    listener_move = null;
+    listener_up = null;
+}
+
+// Event/workspace
+
+function screenOnMouseClick(event) {
+    // if not glab block
+    // this will fix -> create new global variant for judge of clicking object
+    if(listener_move == null && listener_up == null){
+        listener_move = event => screenOnMouseMove(event);
+        listener_up = event => screenOnMouseUp(event);
+
+        x_dragstart = event.pageX;
+        y_dragstart = event.pageY;
+
+        document.addEventListener("mousemove", listener_move, false);
+        document.addEventListener("mouseup", listener_up, false);
+        document.addEventListener("mouseleave", listener_up, false);
+    }
+}
+
+// maybe make bugs
+function screenOnMouseMove(event) {
+    const workspaceRect = workspace.getBoundingClientRect();
+    let deltaMoveX = event.pageX - x_dragstart;
+    let deltaMoveY = event.pageY - y_dragstart;
+    for (const b of blocks){
+        updatePosition(b, b[BLOCK_X] + deltaMoveX, b[BLOCK_Y] + deltaMoveY);
+    }
+    x_dragstart = event.pageX;
+    y_dragstart = event.pageY;
+    event.preventDefault();
+}
+
+function screenOnMouseUp(event) {
+    document.removeEventListener("mousemove", listener_move, false);
+    document.removeEventListener("mouseup", listener_up, false);
+    document.removeEventListener("mouseleave", listener_up, false);
+
     listener_move = null;
     listener_up = null;
 }
@@ -112,34 +181,46 @@ function getSquareDistance(x1, x2, y1, y2) {
 
 // Button
 function clickGenerator() {
-    blocks.push(createBlock(200, 100, 100, 50));
+    blocks.push(createBlock(200, 100, 100, 50, "normal", 1));
+}
+
+function clickGenerator2(n) {
+    blocks.push(createBlock(200, 100, 100 * n, 50, "longer", n));
 }
 
 function clickEnumerator() {
-    /*let roots = [];
+    let roots = [];
     for (const i of blocks) {
-        if (i.parent === -1) {
-            roots.push(i.idx);
+        if (i[BLOCK_PARENT] === null) {
+            roots.push(i);
         }
     }
-    let s = "";
-    function enumerate(array) {
-        for (const i of array) {
-            s += "(" + i;
-            if (blocks[i].children.length > 0) {
-                s += " ";
-                enumerate(blocks[i].children);
+    console.log(roots);
+    if(roots.length == 0 ){
+        alert("There are no blocks");
+    }else if(roots.length > 1){
+        alert("All blocks aren't connected")
+    }else{
+        let s = "";
+        function enumerate(array) {
+            s += "("
+            for (const block of array){
+                s += block[BLOCK_ID] + " ";
+                if(block[BLOCK_CHILDREN].length > 0){
+                    enumerate(block[BLOCK_CHILDREN]);
+                }
             }
             s += ")";
         }
+        enumerate(roots);
+        alert(s);
     }
-    enumerate(roots);
-    alert(s);
-    */
 }
 
 // Entry point
 window.onload = () => {
     generator.onclick = clickGenerator;
     enumerator.onclick = clickEnumerator;
+
+    workspace.onmousedown = event => screenOnMouseClick(event);
 }
