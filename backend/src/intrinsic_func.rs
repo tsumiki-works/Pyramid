0b82176f0e3cf492b847ed862fa320ffe1ff093d
Shinonome517 <shinonome517@gmail.com>
2022-11-04 22:41:51 +0900

    一通りのテストまで完了

--- backend/src/intrinsic_func.rs
+++ backend/src/intrinsic_func.rs
@@ -9,7 +9,7 @@ pub fn diff(f: fn(f64) -> f64, x: f64) -> Result<f64, String>{
     if ans.is_infinite(){
         return Err(String::from("pyramid backend error: function value must be finite"));
     } else if ans.is_nan(){
-        return Err(String::from("pyramid backend error: invaild operation was performed.(NAN detected)"));
+        return Err(String::from("pyramid backend error: invalid operation was performed.(NAN detected)"));
     } else{
         Ok(ans)
     }
@@ -23,12 +23,15 @@ pub fn integrate(f: fn(f64) -> f64, a: f64, b: f64) -> Result<f64, String>{
     if b.is_infinite() || b.is_nan(){
         return Err(String::from("pyramid backend error: argument must be finite number.(invailed argument detected)"));
     }
+    if a > b{
+        return Err(String::from("pyramid backend error: invalid integration interval"))
+    }
     let split_num = search_split_num(1, f, f64::MAX, f64::INFINITY, a, b)?;
     let ans = ((b - a)/(2.*(split_num as f64)))*(2.*lin_space(a..=b, split_num+1).fold(0., |acc: f64, val| acc + f(val)) - f(a) - f(b));
     if ans.is_infinite(){
         return Err(String::from("pyramid backend error: function value must be finite"));
     } else if ans.is_nan(){
-        return Err(String::from("pyramid backend error: invaild operation was performed.(NAN detected)"));
+        return Err(String::from("pyramid backend error: invalid operation was performed.(NAN detected)"));
     } else{
         Ok(ans)
     }
@@ -40,10 +43,11 @@ fn search_split_num(itr_num: usize, f: fn(f64) -> f64, last_solution: f64, last_
     if solution.is_infinite(){
         return Err(String::from("pyramid backend error: function value must be finite"));
     } else if solution.is_nan(){
-        return Err(String::from("pyramid backend error: invaild operation was performed.(NAN detected)"));
+        return Err(String::from("pyramid backend error: invalid operation was performed.(NAN detected)"));
     }
     let rel_err = if solution == 0. {(solution - last_solution).abs()} else {((solution - last_solution)/solution).abs()};
-    if rel_err <= last_rel_err && itr_num < 23 {
+    if rel_err <= last_rel_err && itr_num < 20 {
+        println!("rel_err{}", rel_err);
         search_split_num(itr_num+1, f, solution, rel_err, a, b)
     }
     else{
