35343c8c0f7e021a2682b67ad49a049d7529d22b
Shinonome517 <shinonome517@gmail.com>
2022-11-11 18:30:49 +0900

    型を実装し、add関数を対応させた

--- backend/src/evaluater.rs
+++ backend/src/evaluater.rs
@@ -1,21 +1,41 @@
 use super::parser::Ast;
 
-pub enum PyramidType{
-    Int(i64),
-    Double(f64),
-    PyramidString(String),
-    Bool(bool),
-    List(std::collections::LinkedList<PyramidType>),
+pub enum PyramidType {
+    Int,
+    Double,
+    PyramidString,
+    Bool,
+    List(Box<PyramidType>),
+    Nil,
 }
 
-pub fn eval(ast: Ast) -> Result<String, String> {
+pub struct PyramidObject {
+    type_id: PyramidType,
+    pub value: Option<String>,
+}
+
+pub fn eval(ast: Ast) -> Result<PyramidObject, String> {
     match ast {
-        Ast::Nil => Ok(String::from("nil")),
-        Ast::Atom(s) => Ok(s),
+        Ast::Nil => Ok(PyramidObject {
+            type_id: PyramidType::Nil,
+            value: None,
+        }),
+        //eval term's type
+        Ast::Atom(s) => {
+            if s == "nil"{
+                Ok(PyramidObject { type_id: PyramidType::Nil, value: None })
+            }else {
+                Ok(PyramidObject {
+                type_id: PyramidType::Int,
+                value: Some(s),
+                })
+            }
+        }
         Ast::Node(car, cdr) => match *car {
             Ast::Atom(s) => {
                 if s == "+" {
                     Ok(eval_operation(*cdr, "+", add)?)
+                // add other calculation
                 } else if s == "nil" {
                     Err(String::from(
                         "pyramid backend error: function symbol must be atom but nil found.",
@@ -35,7 +55,7 @@ pub fn eval(ast: Ast) -> Result<String, String> {
     }
 }
 
-fn add(args: Vec<String>) -> Result<String, String> {
+fn add(args: Vec<PyramidObject>) -> Result<PyramidObject, String> {
     if args.len() != 2 {
         Err(String::from(
             "pyramid backend error: too many arguments for '+' operation.",
@@ -44,22 +64,63 @@ fn add(args: Vec<String>) -> Result<String, String> {
         let mut args = args;
         let arg2 = args.pop().unwrap();
         let arg1 = args.pop().unwrap();
-        let arg2_val = arg2
-            .parse::<i64>()
-            .map_err(|_| format!("pyramid backend error: '{}' is not integer.", arg2))?;
-        let arg1_val = arg1
-            .parse::<i64>()
-            .map_err(|_| format!("pyramid backend error: '{}' is not integer.", arg1))?;
-        let res = arg1_val + arg2_val;
-        Ok(res.to_string())
+        match (arg1.type_id, arg2.type_id) {
+            (PyramidType::PyramidString, PyramidType::PyramidString) => {
+                let arg2_val = arg2.value.clone().unwrap().parse::<String>().map_err(|_| {
+                    format!(
+                        "pyramid backend error: '{}' is not string.",
+                        arg2.value.unwrap()
+                    )
+                })?;
+                let arg1_val = arg1.value.clone().unwrap().parse::<String>().map_err(|_| {
+                    format!("pyramid backend error: '{}' is not integer.", arg1.value.unwrap())
+                })?;
+                let res = arg1_val + arg2_val.as_str();
+                Ok(PyramidObject { type_id: PyramidType::PyramidString, value: Some(res) })
+            }
+            (PyramidType::Int, PyramidType::Int) => {
+                let arg2_val = arg2.value.clone().unwrap().parse::<i64>().map_err(|_| {
+                    format!("pyramid backend error: '{}' is not integer.", arg2.value.unwrap())
+                })?;
+                let arg1_val = arg1.value.clone().unwrap().parse::<i64>().map_err(|_| {
+                    format!("pyramid backend error: '{}' is not integer.", arg1.value.unwrap())
+                })?;
+                let res = arg1_val + arg2_val;
+                Ok(PyramidObject { type_id: PyramidType::Int, value: Some(res.to_string()) })
+            }
+            (PyramidType::Int, PyramidType::Double) | (PyramidType::Double, PyramidType::Int) | (PyramidType::Double, PyramidType::Double) => {
+                let arg2_val = arg2.value.clone().unwrap().parse::<f64>().map_err(|_| {
+                    format!(
+                        "pyramid backend error: '{}' is not float point number.",
+                        arg2.value.unwrap()
+                    )
+                })?;
+                let arg1_val = arg1.value.clone().unwrap().parse::<f64>().map_err(|_| {
+                    format!(
+                        "pyramid backend error: '{}' is not float point number.",
+                        arg1.value.unwrap()
+                    )
+                })?;
+                let res = arg1_val + arg2_val;
+                Ok(PyramidObject { type_id: PyramidType::Double, value: Some(res.to_string()) })
+            }
+            (PyramidType::Nil, _) | (_, PyramidType::Nil) => {
+                Err(String::from("pyramid backend error: nil is not operand."))
+            }
+            _ => Err(String::from(format!(
+                "pyramid backend error: These operands ('{}', '{}') are invaild arguments for 'add'.",
+                arg1.value.unwrap(),
+                arg2.value.unwrap()
+            ))),
+        }
     }
 }
 
 fn eval_operation(
     ast: Ast,
     opname: &'static str,
-    op: fn(_: Vec<String>) -> Result<String, String>,
-) -> Result<String, String> {
+    op: fn(_: Vec<PyramidObject>) -> Result<PyramidObject, String>,
+) -> Result<PyramidObject, String> {
     let mut args = Vec::new();
     let mut cdr = ast;
     loop {
--- backend/src/intrinsic_func.rs
+++ backend/src/intrinsic_func.rs
@@ -1,56 +1,92 @@
 use iter_num_tools::lin_space;
 
-pub fn diff(f: fn(f64) -> f64, x: f64) -> Result<f64, String>{
-    if x.is_infinite() || x.is_nan(){
-        return Err(String::from("pyramid backend error: argument must be finite number.(invailed argument detected)"));
+pub fn diff(f: fn(f64) -> f64, x: f64) -> Result<f64, String> {
+    if x.is_infinite() || x.is_nan() {
+        return Err(String::from(
+            "pyramid backend error: argument must be finite number.(invailed argument detected)",
+        ));
     }
     let h = f64::EPSILON;
-    let ans = (f(x + h) - f(x - h))/(2.0*h);
-    if ans.is_infinite(){
-        return Err(String::from("pyramid backend error: function value must be finite"));
-    } else if ans.is_nan(){
-        return Err(String::from("pyramid backend error: invalid operation was performed.(NAN detected)"));
-    } else{
+    let ans = (f(x + h) - f(x - h)) / (2.0 * h);
+    if ans.is_infinite() {
+        return Err(String::from(
+            "pyramid backend error: function value must be finite",
+        ));
+    } else if ans.is_nan() {
+        return Err(String::from(
+            "pyramid backend error: invalid operation was performed.(NAN detected)",
+        ));
+    } else {
         Ok(ans)
     }
 }
 
 //linspace: https://docs.rs/itertools-num/latest/itertools_num/fn.linspace.html
-pub fn integrate(f: fn(f64) -> f64, a: f64, b: f64) -> Result<f64, String>{
-    if a.is_infinite() || a.is_nan(){
-        return Err(String::from("pyramid backend error: argument must be finite number.(invailed argument detected)"));
+pub fn integrate(f: fn(f64) -> f64, a: f64, b: f64) -> Result<f64, String> {
+    if a.is_infinite() || a.is_nan() {
+        return Err(String::from(
+            "pyramid backend error: argument must be finite number.(invailed argument detected)",
+        ));
     }
-    if b.is_infinite() || b.is_nan(){
-        return Err(String::from("pyramid backend error: argument must be finite number.(invailed argument detected)"));
+    if b.is_infinite() || b.is_nan() {
+        return Err(String::from(
+            "pyramid backend error: argument must be finite number.(invailed argument detected)",
+        ));
     }
-    if a > b{
-        return Err(String::from("pyramid backend error: invalid integration interval"))
+    if a > b {
+        return Err(String::from(
+            "pyramid backend error: invalid integration interval",
+        ));
     }
     let split_num = search_split_num(1, f, f64::MAX, f64::INFINITY, a, b)?;
-    let ans = ((b - a)/(2.*(split_num as f64)))*(2.*lin_space(a..=b, split_num+1).fold(0., |acc: f64, val| acc + f(val)) - f(a) - f(b));
-    if ans.is_infinite(){
-        return Err(String::from("pyramid backend error: function value must be finite"));
-    } else if ans.is_nan(){
-        return Err(String::from("pyramid backend error: invalid operation was performed.(NAN detected)"));
-    } else{
+    let ans = ((b - a) / (2. * (split_num as f64)))
+        * (2. * lin_space(a..=b, split_num + 1).fold(0., |acc: f64, val| acc + f(val))
+            - f(a)
+            - f(b));
+    if ans.is_infinite() {
+        return Err(String::from(
+            "pyramid backend error: function value must be finite",
+        ));
+    } else if ans.is_nan() {
+        return Err(String::from(
+            "pyramid backend error: invalid operation was performed.(NAN detected)",
+        ));
+    } else {
         Ok(ans)
     }
 }
 
-fn search_split_num(itr_num: usize, f: fn(f64) -> f64, last_solution: f64, last_rel_err: f64, a: f64, b: f64) -> Result<usize, String>{
+fn search_split_num(
+    itr_num: usize,
+    f: fn(f64) -> f64,
+    last_solution: f64,
+    last_rel_err: f64,
+    a: f64,
+    b: f64,
+) -> Result<usize, String> {
     let split_num = 2_usize.pow(itr_num as u32);
-    let solution = ((b - a)/(2.*(split_num as f64)))*(2.*lin_space(a..=b, split_num+1).fold(0., |acc: f64, val| acc + f(val)) - f(a) - f(b));
-    if solution.is_infinite(){
-        return Err(String::from("pyramid backend error: function value must be finite"));
-    } else if solution.is_nan(){
-        return Err(String::from("pyramid backend error: invalid operation was performed.(NAN detected)"));
+    let solution = ((b - a) / (2. * (split_num as f64)))
+        * (2. * lin_space(a..=b, split_num + 1).fold(0., |acc: f64, val| acc + f(val))
+            - f(a)
+            - f(b));
+    if solution.is_infinite() {
+        return Err(String::from(
+            "pyramid backend error: function value must be finite",
+        ));
+    } else if solution.is_nan() {
+        return Err(String::from(
+            "pyramid backend error: invalid operation was performed.(NAN detected)",
+        ));
     }
-    let rel_err = if solution == 0. {(solution - last_solution).abs()} else {((solution - last_solution)/solution).abs()};
+    let rel_err = if solution == 0. {
+        (solution - last_solution).abs()
+    } else {
+        ((solution - last_solution) / solution).abs()
+    };
     if rel_err <= last_rel_err && itr_num < 20 {
         println!("rel_err{}", rel_err);
-        search_split_num(itr_num+1, f, solution, rel_err, a, b)
-    }
-    else{
+        search_split_num(itr_num + 1, f, solution, rel_err, a, b)
+    } else {
         Ok(2_usize.pow(itr_num as u32 - 1))
     }
 }
