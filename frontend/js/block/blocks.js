57efd06872798dba9682b19aba5e79c2a6e3c22d
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2022-11-14 03:28:41 +0900

    nice block arrangement

--- frontend/js/block/blocks.js
+++ frontend/js/block/blocks.js
@@ -11,30 +11,125 @@ function get_roots() {
     return roots;
 }
 /**
- * A function to remove block from tree in roots array.
- * @param {object} block which you want to remove from trees in roots array
+ * A function to remove all empty block from the top of roots array.
+ * <b>You should call this before push block requests in roots.</b>
  * @memberOf block.blocks
  */
-function remove_block_from_roots(block) {
-    function remove_block_(children) {
-        if (children.length == 0)
+function clean_roots() {
+    roots = roots.filter(block => !is_empty_block(block));
+}
+/**
+ * A function to arrange `block`.
+ * @param {object} target_block 
+ * @param {float} wr 
+ * @param {float} hr 
+ * @memberOf block.blocks
+ */
+function arrange_block(target_block, wr, hr) {
+    /**
+     * A function to determine block width.
+     * Then, block.x is overwritten as offset from center of its area.
+     * @param {object} block 
+     * @memberOf block.blocks
+     * @access private
+     */
+    function determine_block_width(block) {
+        if (is_empty_block(block) || block.children.length == 0) {
+            block.x = 0.0;
+            block.width = BLOCK_UNIT_WIDTH;
+            block.leftmost = -BLOCK_UNIT_WIDTH * 0.5;
+            block.rightmost = BLOCK_UNIT_WIDTH * 0.5;
             return;
-        for (let i = 0; i < children.length; ++i) {
-            if (is_empty_block(children[i]))
-                continue;
-            if (children[i] === block) {
-                if(is_empty_block(children[i].parent)){
-                    roots = roots.filter(_block => block !== _block);
-                    return;
-                }else{
-                    children[i] = {};
-                    return;
-                }
+        }
+        if (block.children.length == 1) {
+            determine_block_width(block.children[0]);
+            block.x = block.children[0].x;
+            block.width = BLOCK_UNIT_WIDTH;
+            block.leftmost = block.children[0].leftmost;
+            block.rightmost = block.children[0].rightmost;
+            return;
+        }
+        block.width = 1.0;
+        block.leftmost = 0.0;
+        block.rightmost = 0.0;
+        let i = 0;
+        for (const child of block.children) {
+            determine_block_width(child);
+            block.leftmost += child.leftmost;
+            block.rightmost += child.rightmost;
+            if (i == 0) {
+                block.width += child.rightmost - child.x - child.width * 0.5;
+            } else if (i == block.children.length - 1) {
+                block.width += child.x - child.width * 0.5 - child.leftmost;
+            } else {
+                block.width += child.rightmost - child.leftmost;
             }
-            remove_block_(children[i].children);
+            i += 1;
+        }
+        block.x = block.leftmost
+            + (block.children[0].x - block.children[0].leftmost)
+            + (block.children[0].width * 0.5 - 0.5)
+            + block.width * 0.5;
+    }
+    /**
+     * A function to determine block position.
+     * Then, block.x must be offset from center of its area.
+     * @param {object} block 
+     * @param {float} x
+     * @param {float} y 
+     * @memberOf block.blocks
+     * @access private
+     */
+    function determine_block_pos(block, x, y) {
+        const center = x - block.x * wr;
+        block.x = x;
+        block.y = y;
+        let offset = center + block.leftmost * wr;
+        for (const child of block.children) {
+            const child_area = (child.rightmost - child.leftmost) * wr;
+            determine_block_pos(child, offset + child_area * 0.5 + child.x * wr, y - BLOCK_HEIGHT * hr);
+            offset += child_area;
         }
     }
-    remove_block_(roots);
+    // from now on
+    const x = target_block.x;
+    const y = target_block.y;
+    determine_block_width(target_block);
+    determine_block_pos(target_block, x, y);
+}
+/**
+ * A function to remove block from tree in `blocks`.
+ * @param {object} target_block which you want to remove
+ * @returns if removed then true, otherwise false.
+ * @memberOf block.blocks
+ */
+function remove_block_from_roots(target_block) {
+    function inner(block) {
+        if (is_empty_block(block)) {
+            return false;
+        }
+        for (let i = 0; i < block.children.length; ++i) {
+            if (block.children[i] === target_block) {
+                block.children[i] = create_empty_block();
+                block.children[i].parent = block;
+                return true;
+            }
+            if (inner(block.children[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+    for (let i = 0; i < roots.length; ++i) {
+        if (roots[i] === target_block) {
+            roots[i] = create_empty_block();
+            return true;
+        }
+        if (inner(roots[i])) {
+            return true;
+        }
+    }
+    return false;
 }
 /**
  * A function to find a block that f(block) is true.
@@ -44,34 +139,22 @@ function remove_block_from_roots(block) {
  * @memberOf block.blocks
  */
 function find_block(blocks, f) {
-    if (blocks.length == 0)
-        return {};
+    if (blocks.length == 0) {
+        return create_empty_block();
+    }
     for (const block of blocks) {
-        if (is_empty_block(block))
+        if (is_empty_block(block)) {
             continue;
-        if (f(block))
+        }
+        if (f(block)) {
             return block;
+        }
         const res_finding_from_children = find_block(block.children, f);
-        if (Object.keys(res_finding_from_children).length != 0)
+        if (!is_empty_block(remove_block_from_roots)) {
             return res_finding_from_children;
+        }
     }
-    return {};
-}
-/**
- * A function to do f for blocks.
- * @param {object[]} blocks which you want to proceed
- * @param {function(object):void} f which you want to apply to all block in blocks
- * @memberOf block.blocks
- */
-function for_each_blocks(blocks, f) {
-    if (blocks.length == 0)
-        return;
-    for (const block of blocks) {
-        if (is_empty_block(block))
-            continue;
-        f(block);
-        for_each_blocks(block.children, f);
-    }
+    return create_empty_block();
 }
 /**
  * A function to push all block entity requests in `blocks`.
@@ -92,7 +175,7 @@ function push_requests_blocks(blocks, is_ui, requests) {
      * @memberOf block.blocks
      * @access private
      */
-    function entity_block(x, y, width, height, col, is_ui) {
+    function entity_block(x, y, width, height, col) {
         return [
             [x, y, 0.0],
             [width, height, 1.0],
@@ -103,87 +186,24 @@ function push_requests_blocks(blocks, is_ui, requests) {
         ];
     }
     /**
-     * A function to arrange blocks.
-     * It determines their width based on children and position based on root.
-     * @param {object[]} blocks which you want to arrange all
-     * @param {float} wr width magnification 
-     * @param {float} hr height magnification 
-     * @memberOf block.arrange
+     * A function to push block requests.
+     * In some cases, empty block is drawn.
+     * @param {object} block
+     * @memberOf block.blocks
      * @access private
      */
-    function arrange_blocks(blocks, wr, hr) {
-        /**
-         * A function to arrange block width based on its children.
-         * For example, let `Block` be `Block(width, children array)`:
-         *   * `Block(1, [Block(1, [])])`
-         *   * `Block(2, [emptyblock, Block(1, [])])`
-         *   * `Block(3, [Block(1, []), Block(1, [Block(1, []), emptyblock])])`
-         * @param {object} block which you want to arrange
-         * @returns {float} sum of width of children
-         * @access private
-         */
-        function arrange_block_width(block) {
-            if (is_empty_block(block))
-                return BLOCK_UNIT_WIDTH;
-            if (block.children_num == 0) {
-                block.width = BLOCK_UNIT_WIDTH;
-                block.children_connection = get_block_connection(block);
-                return block.width;
-                //return BLOCK_UNIT_WIDTH;
-            }
-            const children_width = block.children.reduce(
-                (res, child) => res + arrange_block_width(child),
-                0
-            );
-            block.width = children_width;
-            block.children_connection = get_block_connection(block);
-            if (children_width < block.children_num) {
-                alert(
-                    "pyramid frontend exception: number of children is "
-                    + block.children_num
-                    + " but sum of their width is "
-                    + children_width
-                    + " so something is wrong with system.");
-            }
-            return children_width;
-        }
-        blocks.forEach(block => arrange_block_width(block));
-        for_each_blocks(blocks, block => {
-            if (block.children_num == 0)
-                return;
-            let x = block.x - block.width * 0.5 * wr;
-            for (const child of block.children) {
-                if (is_empty_block(child)) {
-                    x += BLOCK_UNIT_WIDTH;
-                } else {
-                    const c = child.width * 0.5 * wr;
-                    x += c;
-                    child.x = x;
-                    child.y = block.y - BLOCK_HEIGHT * hr;
-                    x += c;
-                }
-            }
-        });
-    }
-    // from now on
-    let wr = 1.0;
-    let hr = 1.0;
-    if (is_ui) {
-        const pos_clipping_1 = convert_view_to_clipping([-0.5, -0.5, camera[2], 1.0], canvas.width, canvas.height);
-        const pos_clipping_2 = convert_view_to_clipping([0.5, 0.5, camera[2], 1.0], canvas.width, canvas.height);
-        wr = canvas.width * 0.5 * (pos_clipping_2[0] / pos_clipping_2[3] - pos_clipping_1[0] / pos_clipping_1[3]);
-        hr = canvas.height * 0.5 * (pos_clipping_2[1] / pos_clipping_2[3] - pos_clipping_1[1] / pos_clipping_2[3]);
-    }
-    arrange_blocks(blocks, wr, hr);
-    for_each_blocks(blocks, block => {
+    function push_requests(block) {
+        if (is_empty_block(block) && is_empty_block(holding_block))
+            return;
+        if (is_empty_block(block) && is_ui)
+            return;
         requests.push(
             entity_block(
                 block.x,
                 block.y,
-                Math.max(block.width - 0.6, 1.0) * wr,
+                block.width * wr,
                 BLOCK_HEIGHT * hr,
-                TYPE_TO_COL[block.type],
-                is_ui
+                is_empty_block(block) ? [1.0, 0.0, 0.0, 0.5] : TYPE_TO_COL[block.type]
             )
         );
         push_requests_text(
@@ -196,5 +216,21 @@ function push_requests_blocks(blocks, is_ui, requests) {
             is_ui,
             requests
         );
-    });
+        for (const child of block.children) {
+            push_requests(child);
+        }
+    }
+    // from now on
+    let wr = 1.0;
+    let hr = 1.0;
+    if (is_ui) {
+        const pos_clipping_1 = convert_view_to_clipping([-0.5, -0.5, camera[2], 1.0], canvas.width, canvas.height);
+        const pos_clipping_2 = convert_view_to_clipping([0.5, 0.5, camera[2], 1.0], canvas.width, canvas.height);
+        wr = canvas.width * 0.5 * (pos_clipping_2[0] / pos_clipping_2[3] - pos_clipping_1[0] / pos_clipping_1[3]);
+        hr = canvas.height * 0.5 * (pos_clipping_2[1] / pos_clipping_2[3] - pos_clipping_1[1] / pos_clipping_2[3]);
+    }
+    for (const block of blocks) {
+        arrange_block(block, wr, hr);
+        push_requests(block);
+    }
 }
