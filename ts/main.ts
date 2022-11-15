f330d492b7860ca9276ea0ba69330189379ea614
mugicha9 <jkn.mugicha@gmail.com>
2022-11-16 01:10:57 +0900

    commit for merge

--- ts/main.ts
+++ ts/main.ts
@@ -3,10 +3,20 @@ import {Block} from "./block/block.js"
 
 export class Pyramid {
     private canvas: HTMLCanvasElement;
+
+    camera: number[] = [0.0, 0.0, -5.0];
+
     constructor () {
         this.canvas = document.getElementById("workspace") as HTMLCanvasElement;
     }
     run(): void {
-        
+        let test: Block = new Block(200, 400, 0, "0");
+        let test2: Block = new Block();
+        test.debug();
+        test2.debug();
+    }
+
+    render(): void {
+
     }
 }
---
+++ ts/screen/canvas.ts
@@ -0,0 +1,8 @@
+/**
+ * A module to control all contents on the canvas
+ */
+export class PyramidCanvas {
+    /*
+    call console, event, popup and workspace
+    */
+}
