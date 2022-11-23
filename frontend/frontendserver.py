45722f4740d66705e793c15144afb522da40ed05
mugicha9 <jkn.mugicha@gmail.com>
2022-11-23 17:32:22 +0900

    delete un-use files

--- frontend/frontendserver.py
+++
@@ -1,8 +0,0 @@
-import http.server
-import socketserver
-
-PORT = 8000
-handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    httpd.serve_forever()
--- frontend/js/pagetransitions.js
+++
@@ -1,7 +0,0 @@
-function goto_toppage() {
-    window.location.href = "index.html"
-}
-
-function goto_doc() { // 技術者向けページ？
-    window.location.href = "doc"
-}
