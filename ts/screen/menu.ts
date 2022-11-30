261de06bb7b49f8bf2abf7c85a82aaa621cd446a
mugicha9 <jkn.mugicha@gmail.com>
2022-12-01 03:22:04 +0900

    form menu.ts

---
+++ ts/screen/menu.ts
@@ -0,0 +1,32 @@
+import { CanvasItem } from "./canvas_items.js";
+import { ImageTexture } from "../webgl/image_texture.js";
+import { Vec3, Vec4 } from "../webgl/math.js";
+import { Request } from "../webgl/request.js";
+
+export class Menu {
+    private menu_items: CanvasItem[];
+
+    constructor(){}
+
+    /* 
+    create push_menublock_requests(view: Vec3): Request {
+
+    }
+    */
+
+    /*
+    craete push_request_menu(view: Vec3): Request {
+
+    }
+    */
+
+    /*
+    create push_request_lines(view: Vec3): Request {
+        
+    }
+    */
+
+    push_requests(view: Vec3, requests: Request[]): void {
+        
+    }
+}
