07428801700ea06a4db7816a7b3e1d1543ffa7a4
Shinonome517 <shinonome517@gmail.com>
2022-12-07 17:41:50 +0900

    EmptyBlockにイベントを追加した

--- ts/block/concrete_block/empty-block.ts
+++ ts/block/concrete_block/empty-block.ts
@@ -6,9 +6,21 @@ export class EmptyBlock extends Block {
                 type_id: PyramidTypeID.Empty,
                 attribute: null
             });
+        this.init_events();
     }
     kill(): void {
         this.remove();
+    }
+    
+    private mousedown_listener: EventListener;
+
+    private init_events() {
+        this.mousedown_listener = (e: MouseEvent) => this.event_mousedown(e);
+        this.addEventListener("mousedown", this.mousedown_listener);
+    }
+
+    private event_mousedown(e: MouseEvent){
+        e.stopPropagation();
     }
 }
 customElements.define('pyramid-block-empty', EmptyBlock);
