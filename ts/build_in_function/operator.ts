706eeaceeddabf0420ca65d380c02a55f6717244
Shinonome517 <shinonome517@gmail.com>
2022-12-12 00:17:57 +0900

    算術演算子の実装とテストが終了

--- ts/build_in_function/operator.ts
+++ ts/build_in_function/operator.ts
@@ -24,12 +24,12 @@ export class Operator {
     static add(arg1: PyramidObject, arg2: PyramidObject): PyramidObject {
         if (arg1.pyramid_type.type_id === PyramidTypeID.I32 && arg2.pyramid_type.type_id === PyramidTypeID.I32) {
             let ans: number = arg1.value + arg2.value;
-            //check isNaN, isInf, isInt
-            if (Number.isNaN(ans)) throw Error("pyramid backend error: int addition return NaN");
-            if (!Number.isInteger(ans)) throw Error("pyramid backend error: int addition return non-integer")
+            //check isNaN, isInt, isInf
+            if (Number.isNaN(ans)) throw new Error("pyramid backend error: int addition return NaN");
+            if (!Number.isInteger(ans)) throw new Error("pyramid backend error: int addition return non-integer")
             if (!Operator.is_in_i32(ans)) {
-                if (ans >= 0) throw Error("pyramid backend error: int addition overflowed toward positive");
-                else throw Error("pyramid backend error: int addition overflowed toward negative");
+                if (ans >= 0) throw new Error("pyramid backend error: int addition overflowed toward positive");
+                else throw new Error("pyramid backend error: int addition overflowed toward negative");
             }
             return {
                 pyramid_type: { type_id: PyramidTypeID.I32, attribute: null },
@@ -54,8 +54,8 @@ export class Operator {
         else if (arg1.pyramid_type.type_id === PyramidTypeID.String && arg2.pyramid_type.type_id === PyramidTypeID.String) {
             let ans: string = arg1.value + arg2.value;
             //check isNaN isString
-            if (Number.isNaN(ans)) throw Error("pyramid backend error: string addition return NaN")
-            if (!Operator.isString(ans)) throw Error("pyramid backend error: string addition return non-string")
+            if (Number.isNaN(ans)) throw new Error("pyramid backend error: string addition return NaN")
+            if (!Operator.isString(ans)) throw new Error("pyramid backend error: string addition return non-string")
             return {
                 pyramid_type: { type_id: PyramidTypeID.String, attribute: null },
                 value: ans
@@ -68,12 +68,12 @@ export class Operator {
     static sub(arg1: PyramidObject, arg2: PyramidObject): PyramidObject {
         if (arg1.pyramid_type.type_id === PyramidTypeID.I32 && arg2.pyramid_type.type_id === PyramidTypeID.I32) {
             let ans: number = arg1.value - arg2.value;
-            //check isNaN, isInf, isInt
-            if (Number.isNaN(ans)) throw Error("pyramid backend error: int subtraction return NaN");
-            if (!Number.isInteger(ans)) throw Error("pyramid backend error: int subtraction return non-integer")
+            //check isNaN, isInt, isInf
+            if (Number.isNaN(ans)) throw new Error("pyramid backend error: int subtraction return NaN");
+            if (!Number.isInteger(ans)) throw new Error("pyramid backend error: int subtraction return non-integer")
             if (!Operator.is_in_i32(ans)) {
-                if (ans >= 0) throw Error("pyramid backend error: int subtraction overflowed toward positive");
-                else throw Error("pyramid backend error: int subtraction overflowed toward negative");
+                if (ans >= 0) throw new Error("pyramid backend error: int subtraction overflowed toward positive");
+                else throw new Error("pyramid backend error: int subtraction overflowed toward negative");
             }
             return {
                 pyramid_type: { type_id: PyramidTypeID.I32, attribute: null },
@@ -97,6 +97,142 @@ export class Operator {
         }
         else {
             throw new Error("pyramid backend error: invalid operands are given sub oprator")
+        }
+    }
+    static mul(arg1: PyramidObject, arg2: PyramidObject): PyramidObject {
+        if (arg1.pyramid_type.type_id === PyramidTypeID.I32 && arg2.pyramid_type.type_id === PyramidTypeID.I32) {
+            let ans: number = arg1.value * arg2.value;
+            //check isNaN, isInt, isInf
+            if (Number.isNaN(ans)) throw new Error("pyramid backend error: int multiplication return NaN");
+            if (!Number.isInteger(ans)) throw new Error("pyramid backend error: int multiplication return non-integer")
+            if (!Operator.is_in_i32(ans)) {
+                if (ans >= 0) throw new Error("pyramid backend error: int multiplication overflowed toward positive");
+                else throw new Error("pyramid backend error: int multiplication overflowed toward negative");
+            }
+            return {
+                pyramid_type: { type_id: PyramidTypeID.I32, attribute: null },
+                value: ans
+            }
+        }
+        else if (arg1.pyramid_type.type_id === PyramidTypeID.F32 && arg2.pyramid_type.type_id === PyramidTypeID.F32
+            || arg1.pyramid_type.type_id === PyramidTypeID.F32 && arg2.pyramid_type.type_id === PyramidTypeID.I32
+            || arg1.pyramid_type.type_id === PyramidTypeID.I32 && arg2.pyramid_type.type_id === PyramidTypeID.F32) {
+            let ans: number = arg1.value * arg2.value;
+            //check isNaN isInf
+            if (Number.isNaN(ans)) return Operator.pyramid_f32_nan;
+            if (!Operator.is_in_f32(ans)) {
+                if (ans >= 0) return Operator.pyramid_f32_positive_inf;
+                else return Operator.pyramid_f32_negative_inf;
+            }
+            return {
+                pyramid_type: { type_id: PyramidTypeID.F32, attribute: null },
+                value: ans
+            }
+        }
+        else {
+            throw new Error("pyramid backend error: invalid operands are given mul oprator")
+        }
+    }
+    static div(arg1: PyramidObject, arg2: PyramidObject): PyramidObject {
+        if (arg1.pyramid_type.type_id === PyramidTypeID.I32 && arg2.pyramid_type.type_id === PyramidTypeID.I32) {
+            let ans: number = arg1.value / arg2.value;
+            //check zero division, isNaN, isInt, isInf
+            if (arg2.value === 0) throw new Error("pyramid backend error: int division deteched zero division")
+            //The div operator allows taking an Int type and returning a Float type.
+            if (Number.isNaN(ans)) throw new Error("pyramid backend error: int division return NaN");
+            if (!Number.isInteger(ans)) {
+                if (!Operator.is_in_f32(ans)) {
+                    if (ans >= 0) return Operator.pyramid_f32_positive_inf;
+                    else return Operator.pyramid_f32_negative_inf;
+                }
+                return {
+                    pyramid_type: { type_id: PyramidTypeID.F32, attribute: null },
+                    value: ans
+                }
+            }
+            else {
+                if (!Operator.is_in_i32(ans)) {
+                    if (ans >= 0) throw new Error("pyramid backend error: int division overflowed toward positive");
+                    else throw new Error("pyramid backend error: int division overflowed toward negative");
+                }
+                return {
+                    pyramid_type: { type_id: PyramidTypeID.I32, attribute: null },
+                    value: ans
+                }
+            }
+        }
+        else if (arg1.pyramid_type.type_id === PyramidTypeID.F32 && arg2.pyramid_type.type_id === PyramidTypeID.F32
+            || arg1.pyramid_type.type_id === PyramidTypeID.F32 && arg2.pyramid_type.type_id === PyramidTypeID.I32
+            || arg1.pyramid_type.type_id === PyramidTypeID.I32 && arg2.pyramid_type.type_id === PyramidTypeID.F32) {
+            let ans: number = arg1.value / arg2.value;
+            //check isNaN isInf
+            if (Number.isNaN(ans)) return Operator.pyramid_f32_nan;
+            if (!Operator.is_in_f32(ans)) {
+                if (ans >= 0) return Operator.pyramid_f32_positive_inf;
+                else return Operator.pyramid_f32_negative_inf;
+            }
+            return {
+                pyramid_type: { type_id: PyramidTypeID.F32, attribute: null },
+                value: ans
+            }
+        }
+        else {
+            throw new Error("pyramid backend error: invalid operands are given div oprator")
+        }
+    }
+    static mod(arg1: PyramidObject, arg2: PyramidObject): PyramidObject {
+        if (arg1.pyramid_type.type_id === PyramidTypeID.I32 && arg2.pyramid_type.type_id === PyramidTypeID.I32) {
+            let ans: number = arg1.value % arg2.value;
+            //check isNaN, isInt, isInf
+            //The div operator allows taking an Int type and returning a Float type.
+            if (Number.isNaN(ans)) throw new Error("pyramid backend error: int modulo operation return NaN");
+            if (!Number.isInteger(ans)) throw new Error("pyramid backend error: int modulo operation return non-integer")
+            if (!Operator.is_in_i32(ans)) {
+                if (ans >= 0) throw new Error("pyramid backend error: int modulo operation overflowed toward positive");
+                else throw new Error("pyramid backend error: int modulo operation overflowed toward negative");
+            }
+            return {
+                pyramid_type: { type_id: PyramidTypeID.I32, attribute: null },
+                value: ans
+            }
+        }
+        //TODO:(Resolved)
+        //Whether modulo operation for float type should be allowed or not
+        //At TypeScript, 5.2 % 3 returns 2.2 
+        //(Note: Number.isInterger(5.2 % 3) returns false and Number.isInteger(5.0 % 3.0) returns true)
+        
+        //Because of this unreasonable specification, forbit module operation for float type
+        
+        else if (arg1.pyramid_type.type_id === PyramidTypeID.F32 && arg2.pyramid_type.type_id === PyramidTypeID.F32
+            || arg1.pyramid_type.type_id === PyramidTypeID.F32 && arg2.pyramid_type.type_id === PyramidTypeID.I32
+            || arg1.pyramid_type.type_id === PyramidTypeID.I32 && arg2.pyramid_type.type_id === PyramidTypeID.F32) {
+            let ans: number = arg1.value % arg2.value;
+            //check isNaN isInt isInf
+            if (Number.isNaN(ans)) throw new Error("pyramid backend error: float modulo operation return NaN");
+            if (!Number.isInteger(ans)) {
+                if (!Operator.is_in_f32(ans)) {
+                    if (ans >= 0) return Operator.pyramid_f32_positive_inf;
+                    else return Operator.pyramid_f32_negative_inf;
+                }
+                return {
+                    pyramid_type: { type_id: PyramidTypeID.F32, attribute: null },
+                    value: ans
+                }
+            }
+            else {
+                if (!Operator.is_in_i32(ans)) {
+                    if (ans >= 0) throw new Error("pyramid backend error: float modulo operation overflowed toward positive");
+                    else throw new Error("pyramid backend error: float modulo operation overflowed toward negative");
+                }
+                return {
+                    pyramid_type: { type_id: PyramidTypeID.I32, attribute: null },
+                    value: ans
+                }
+            }
+        }
+        //*/
+        else {
+            throw new Error("pyramid backend error: invalid operands are given mod oprator")
         }
     }
 }
