dcf7ab775687026b349eb92de5ea27466f6a3866
Shinonome517 <shinonome517@gmail.com>
2022-12-15 13:34:58 +0900

    組み込み関数群をevaluationモジュールへ移動させた

---
+++ ts/evaluation/list_func.ts
@@ -0,0 +1,14 @@
+/*
+class ListFunc{
+    static map(fn: PyramidObject, lst: PyramidObject): PyramidObject{
+        if(fn.pyramid_type.type_id === PyramidTypeID.Function 
+        && fn.pyramid_type.attribute === 2 //FIXME: Maybe, this conditional expression never need.
+        && lst.pyramid_type.type_id === PyramidTypeID.List){
+            //
+        }
+        else{
+            new Error("pyramid backend error: invalid operands are given map function")
+        }
+    }
+}
+*/
