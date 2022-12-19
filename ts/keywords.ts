664e98e4ac5105376643d2e0ca5234b55e4bf624
Tengu712 <genreihoutengu@gmail.com>
2022-12-20 05:09:12 +0900

    checked recursion

--- ts/keywords.ts
+++ ts/keywords.ts
@@ -5,11 +5,11 @@ import { MathFunction } from "./evaluation/math_function.js";
 
 export const keywords: [string, TempPyramidType, any][] = [
     ["+", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.add],
-    ["-", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.add],
-    ["*", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.add],
-    ["/", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.add],
-    ["%", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.add],
-    ["**", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.add],
+    ["-", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.sub],
+    ["*", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.mul],
+    ["/", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.div],
+    ["%", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.mod],
+    ["**", ArithmeticOperator.typeof_arythmetic_operator, ArithmeticOperator.pow],
 
     ["!", LogicalOperator.typeof_not, LogicalOperator.not],
     ["&&", LogicalOperator.typeof_logical_binop, LogicalOperator.and],
