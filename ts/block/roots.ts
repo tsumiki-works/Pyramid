ae7348551770a200f1c1dacf9ecf7cd4ea6bdb79
Tengu712 <genreihoutengu@gmail.com>
2022-12-21 13:26:31 +0900

    added global scope

--- ts/block/roots.ts
+++ ts/block/roots.ts
@@ -1,4 +1,7 @@
+import { Environment } from "../evaluation/environment.js";
 import { Block } from "./block.js";
+import { DefineBlock } from "./concrete_block/define_block.js";
+import { TypeEnv } from "./inference/typeenv.js";
 
 /* ================================================================================================================= */
 /*     Roots                                                                                                         */
@@ -25,7 +28,31 @@ export class Roots {
             }
         }
     }
+
     static get(): Block[] {
         return Array.from(Roots.roots.children) as Array<Block>;
+    }
+
+    static push_global_definitions(self: Block, env: Environment) {
+        for (const root of Roots.get()) {
+            if (root !== self && root.tagName === "PYRAMID-DEFINE-BLOCK") {
+                const definition = root as DefineBlock;
+                if (definition.get_type().type_id !== PyramidTypeID.Invalid) {
+                    definition.push_definition(env);
+                }
+            }
+        }
+    }
+
+    static push_global_definition_types(self: Block, env: TypeEnv) {
+        for (const root of Roots.get()) {
+            if (root !== self && root.tagName === "PYRAMID-DEFINE-BLOCK") {
+                console.log(root.tagName);
+                const definition = root as DefineBlock;
+                if (definition.get_type().type_id !== PyramidTypeID.Invalid) {
+                    definition.push_definition_type(env);
+                }
+            }
+        }
     }
 }
